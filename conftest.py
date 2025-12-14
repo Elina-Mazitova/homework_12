19593833feccdacefb599a3d2dd317407ed9e64a
Elina Mazitova <elinamazitova14@gmail.com>
2025-12-14 17:05:16 +0300

    add REMOTE_URL to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,28 +1,32 @@
+import os
 import pytest
 import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
+from dotenv import load_dotenv
 
+load_dotenv()
 
 @pytest.fixture(scope='function')
 def setup_browser():
     options = Options()
     options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "128.0")
+    options.set_capability("browserVersion", "127.0")
     options.set_capability("selenoid:options", {
         "enableVNC": True,
         "enableVideo": True
     })
     options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
 
+    remote_url = os.getenv("REMOTE_URL")
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=remote_url,
         options=options
     )
 
-    # browser.config.driver = webdriver.Chrome()
     browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
     browser.config.window_width = 1920
@@ -30,22 +34,9 @@ def setup_browser():
 
     yield browser
 
-    # артефакты для Allure
-    # скриншот
-    allure.attach(
-        driver.get_screenshot_as_png(),
-        name="screenshot",
-        attachment_type=AttachmentType.PNG
-    )
+    allure.attach(driver.get_screenshot_as_png(), name="screenshot", attachment_type=AttachmentType.PNG)
+    allure.attach(driver.page_source, name="page_source", attachment_type=AttachmentType.HTML)
 
-    # HTML страницы
-    allure.attach(
-        driver.page_source,
-        name="page_source",
-        attachment_type=AttachmentType.HTML
-    )
-
-    # логи браузера
     try:
         logs = driver.get_log("browser")
         text = "\n".join([f"{l['level']}: {l['message']}" for l in logs])
@@ -53,7 +44,6 @@ def setup_browser():
     except Exception:
         pass
 
-    # видео
     video_url = f"https://selenoid.autotests.cloud/video/{driver.session_id}.mp4"
     allure.attach(
         f"<html><body><video width='100%' height='100%' controls autoplay>"
